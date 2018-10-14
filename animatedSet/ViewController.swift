6f7d14eb5a48efaf30daf8aab741a785fb62ab45
Anti <anti@antis-imac-pro.local>
2018-10-14 03:03:27 -0700

    Initial Commit

---
+++ animatedSet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animatedSet
+//
+//  Created by Anti on 10/14/18.
+//  Copyright © 2018 Anti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
